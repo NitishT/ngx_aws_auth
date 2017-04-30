d21f74c2452fffc9808e5ded4a9662407c3b462f
Nitish <tiwari.nitish@gmail.com>
2017-04-30 14:04:20 -0700

    add logging

--- ngx_http_aws_auth.c
+++ ngx_http_aws_auth.c
@@ -6,6 +6,13 @@
 #define AWS_S3_VARIABLE "s3_auth_token"
 #define AWS_DATE_VARIABLE "aws_date"
 
+// mainly useful to avoid having to full instantiate request structures for
+// tests...
+#define safe_ngx_log_error(req, ...)                                  \
+  if (req->connection) {                                              \
+    ngx_log_error(NGX_LOG_ERR, req->connection->log, 0, __VA_ARGS__); \
+  }
+  
 static void* ngx_http_aws_auth_create_loc_conf(ngx_conf_t *cf);
 static char* ngx_http_aws_auth_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child);
 static ngx_int_t ngx_aws_auth_req_init(ngx_conf_t *cf);
